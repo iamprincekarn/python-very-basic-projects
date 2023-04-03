f68bc6dc47288e66347c858f0bd0a46c98d2ca36
Prince Kumar <84575785+iamprincekarn@users.noreply.github.com>
2023-04-03 15:49:24 +0530

    Create extract_instagram_data.py

---
+++ extract_instagram_data.py
@@ -0,0 +1,4 @@
+import instaloader
+l = instaloader.Instaloader()
+# l.download_profile('amrita_rao_insta',profile_pic_only=True) #if we want to only download the profile picture
+l.download_profile('amrita_rao_insta') #if we want to download everything on this account
